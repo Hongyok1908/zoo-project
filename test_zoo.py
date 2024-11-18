ab86eb3230743bd1ecb078f79d0bd2795a2f338c
Hongyok1908 <157350481+Hongyok1908@users.noreply.github.com>
2024-11-18 10:23:19 +0000

    Added the test cases

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,20 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_invalid_age(self):
+        self.assertEqual(self.zoo.get_ticket_price(-4), "Age must be more than zero")
+
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(8), 50)
        
-    # Add your additional test cases here.
+    def test_teen_ticket_price(self):
+       self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_adult_ticket_price(self):
+       self.assertEqual(self.zoo.get_ticket_price(40), 150)
+    
+    def test_senior_ticket_price(self):
+       self.assertEqual(self.zoo.get_ticket_price(93), 100)
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,25 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if age <= 0:
+            return "Age must be more than zero"
+        if 0 < age <= 12:      
             return 50
-        elif 13 <= age < 20:
+        elif 12 < age <= 20:   
             return 100
-        elif 21 < age <= 60:
+        elif 20 < age <= 60:   
             return 150
-        elif age >= 60:
+        else:         
             return 100
+        
+
+if __name__ == "__main__":
+    zoo = Zoo()
+    print("Zoo ticket prices : ")
+    test_ages = [5, 15, 30, 70, -1]
+    
+    for age in test_ages:
+        try:
+            price = zoo.get_ticket_price(age)
+            print(f"Age {age}: {price} baht")
+        except ValueError as e:
+            print(f"Age {age}: {e}")
